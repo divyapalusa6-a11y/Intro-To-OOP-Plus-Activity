5290c108a3286569b616de752c7e9578a8db72c7
Ansel Rognlie <ansel@adadevelopersacademy.org>
2026-09-17 00:44:16 -0700

    prefers first student in comparison

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,5 +1,5 @@
 # add your get_student_with_more_classes function here!
 
 def get_student_with_more_classes(student_1, student_2):
-    return student_1.name if len(student_1.classes) > len(student_2.classes) else student_2.name
+    return student_1.name if len(student_1.classes) >= len(student_2.classes) else student_2.name
     
